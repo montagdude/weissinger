bd470f2c0058edd6f2b661eb4eb495fce850ee25
Daniel Prosser <dpross1100@msn.com>
2016-12-24 18:47:35 -0500

    Set default inputs

--- inputs.py
+++ inputs.py
@@ -1,7 +1,7 @@
 alpha   = 5         # Geometric angle of attack at root, degrees
-span    = 200       # Wing span
+span    = 100       # Wing span
 root    = 10        # Root chord
-tip     = 10        # Tip chord
-sweep   = 0         # Sweep of quarter-chord, degrees
-washout = 0         # Downward twist at tip, degrees
+tip     = 7         # Tip chord
+sweep   = 20        # Sweep of quarter-chord, degrees
+washout = 5         # Downward twist at tip, degrees
 npoints = 21        # Number of points to evaluate on wing half
